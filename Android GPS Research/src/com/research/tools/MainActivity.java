d10ee54034912173f9c680ae7ba42c5194f09750
unknown <lp150189@gmail.com>
2014-02-15 19:37:02 -0800

    adding the accelerometer function to the app

--- Android GPS Research/src/com/research/tools/MainActivity.java
+++ Android GPS Research/src/com/research/tools/MainActivity.java
@@ -10,6 +10,10 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
@@ -24,7 +28,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
 	
 private static Context context;
 
@@ -38,10 +42,13 @@ private DeviceListener deviceListener;
 private Timer timer;
 private TimerTask timerTaskOne;
 private TimerTask timerTaskTwo;
+private TimerTask timerTaskThree;
 
 private ToggleButton gpsButton;
 private ToggleButton wifiButton;
 private ToggleButton cellButton;
+private ToggleButton accelButton;
+private SensorManager sensorManager;
 private Button testButton;
 
 
@@ -55,19 +62,21 @@ private Button testButton;
 		gpsButton = (ToggleButton) findViewById(R.id.gps_button);
 		wifiButton = (ToggleButton) findViewById(R.id.wifi_button);
 		cellButton = (ToggleButton) findViewById(R.id.cell_button);
+		accelButton =  (ToggleButton) findViewById(R.id.accelerometer_button);
 		testButton = (Button) findViewById(R.id.test_button);
 		
 		gpsButton.setOnCheckedChangeListener(new GPSButtonListner());
 		wifiButton.setOnCheckedChangeListener(new WifiButtonListner());
 		cellButton.setOnCheckedChangeListener(new CellButtonListner());
 		testButton.setOnClickListener(new TestButtonListener());
-		
-		
+		accelButton.setOnCheckedChangeListener(new AccButtonListner());
 	}
 	public void onGPSButtonClick(){
 		Log.i("b","gps");
 		wifiButton.setChecked(false);
 		cellButton.setChecked(false);
+		accelButton.setChecked(false);
+		
 		if(!gpsOn){
 			CharSequence text = "Test started, please wait " + getTimeToRun() +" minutes.";
 			if(getTimeToRun() < 1){
@@ -129,6 +138,7 @@ private Button testButton;
 		Log.i("b","wifi");
 		gpsButton.setChecked(false);
 		cellButton.setChecked(false);
+		accelButton.setChecked(false);
 		if(!wifiOn){	
 			CharSequence text = "Test started, please wait " + getTimeToRun() +" minutes.";
 			if(getTimeToRun() < 1){
@@ -274,6 +284,8 @@ private Button testButton;
 	public void onCellButtonClick() throws JSONException{
 		gpsButton.setChecked(false);
 		wifiButton.setChecked(false);
+		accelButton.setChecked(false);
+		
 		String[] huy = new String[2];
 		Postmethod postmethod = new Postmethod(this);
 		long startTime = System.currentTimeMillis();
@@ -299,7 +311,6 @@ private Button testButton;
 				firstLockTextView.setText(durationText);
 				lat.setText(Double.toString(lattitude));
 				lon.setText(Double.toString(longtitude));
-				
 				break;
 			}
 		}
@@ -308,6 +319,56 @@ private Button testButton;
 //		Intent intent = new Intent(this, CellActivity.class);
 	    //startActivity(intent);
 	}
+	
+	public void onAccelButtonClick(){
+		gpsButton.setChecked(false);
+		wifiButton.setChecked(false);
+		cellButton.setChecked(false);
+		
+		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL );
+		
+		int duration = Toast.LENGTH_SHORT;
+		Toast toast = Toast.makeText(context, "Test Started", duration);
+		toast.show();
+		
+		deviceListener = new DeviceListener(3);
+		final Handler handler = new Handler();
+		timer = new Timer(false);
+		
+		timerTaskThree = new TimerTask(){
+			@Override
+			public void run() {
+				handler.post(new Runnable() {
+					
+					@Override
+					public void run() {
+						TextView time = (TextView) findViewById(R.id.time_on_acc);
+		        		TextView batChange = (TextView) findViewById(R.id.acc_baterry_lost);	
+		        		time.setText(deviceListener.getTimeRunning());
+		        		batChange.setText(Integer.toString((int)deviceListener.getBatteryChange()) + "%");
+					}
+				});
+			}
+		};
+		timer.scheduleAtFixedRate(timerTaskThree, 0, 1000);
+		
+	}
+	
+	public void onSensorChanged(SensorEvent event){
+		if(event.sensor.getType()== Sensor.TYPE_ACCELEROMETER)
+		{
+			double x = (double) event.values[0];
+			double y = (double) event.values[1];
+			double z = (double) event.values[2];
+			
+			double velocity = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+			TextView velocityText = (TextView) findViewById(R.id.velocityMag);
+			velocityText.setText(String.valueOf(velocity));
+			
+		}
+	}
+	
 	
 	public static Context getContext(){
 		return context;
@@ -353,6 +414,20 @@ private Button testButton;
 			}
 		}
 	}
+	
+	class AccButtonListner implements OnCheckedChangeListener{
+
+		@Override
+		public void onCheckedChanged(CompoundButton button, boolean check) {
+			if(check)
+			{	
+				onAccelButtonClick();
+			}
+			else {
+				resetView();
+			}
+		}
+	}
 	class TestButtonListener implements OnClickListener{
 
 		@Override
@@ -370,4 +445,9 @@ private Button testButton;
 			overridePendingTransition(17432576 , 17432579 );
 			startActivity(intent);
 		}
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+		
+	}
 }
