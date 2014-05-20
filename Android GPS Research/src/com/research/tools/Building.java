847b29a17984931c0ea02e0aacae969b789e6868
unknown <lp150189@gmail.com>
2014-05-19 23:54:19 -0700

    add the building list to parse later on

--- Android GPS Research/src/com/research/tools/Building.java
+++ Android GPS Research/src/com/research/tools/Building.java
@@ -1,16 +1,22 @@
 package com.research.tools;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Building {
 	private LatLng location;
 	private String name;
 	private String events;
-	
+	private MarkerOptions marker;
 	public Building(LatLng location , String name, String events){
 		this.location = location;
 		this.name = name;
 		this.events = events;
+		marker = new MarkerOptions();
+		marker.position(location);
+		marker.title(name);
+		marker.snippet(events);
 	}
 	
 	public Building (LatLng location, String name){
