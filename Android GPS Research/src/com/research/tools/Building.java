57d004891b365965d18224a2388ecd3e0f1061ff
unknown <lp150189@gmail.com>
2014-05-20 17:40:03 -0700

    making the parser

--- Android GPS Research/src/com/research/tools/Building.java
+++ Android GPS Research/src/com/research/tools/Building.java
@@ -5,23 +5,30 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Building {
+	private int id;
 	private LatLng location;
 	private String name;
 	private String events;
 	private MarkerOptions marker;
-	public Building(LatLng location , String name, String events){
+	
+	public Building(int id,LatLng location , String name, String events){
 		this.location = location;
 		this.name = name;
 		this.events = events;
+		this.id = id;
 		marker = new MarkerOptions();
 		marker.position(location);
 		marker.title(name);
 		marker.snippet(events);
 	}
 	
-	public Building (LatLng location, String name){
+	public Building (int id, LatLng location, String name){
 		this.location = location;
 		this.name  = name;
+		this.id = id;
+		marker = new MarkerOptions();
+		marker.position(location);
+		marker.title(name);
 	}
 	
 	public LatLng getLocation(){
@@ -38,5 +45,13 @@ public class Building {
 	
 	public void setEvent(String events){
 		this.events = events;
+	}
+	
+	public int getID (){
+		return id;
+	}
+	
+	public String getString(){
+		return Integer.toString(id) + "\t" + Double.toString(location.latitude)+ "," +Double.toString(location.longitude) +"\t" +name+ "\t"+ events;
 	}
 }
